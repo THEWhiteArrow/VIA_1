82e6b725ae352bd315a981bb654650ba4f8b9503
cj1234567891011 <117266318+cj1234567891011@users.noreply.github.com>
2022-11-02 13:07:01 +0000

    Add files via upload
    Here is the updated version with comments

--- QueueSimulation/QueueSimulation.java
+++ QueueSimulation/QueueSimulation.java
@@ -6,13 +6,15 @@ public class QueueSimulation
 {
   public int simulate(ArrayList<Integer> input)
   {
-    while (input.size()!=1)
+    while (input.size()!=1)//runs 2n times
     {
-      input.remove(0);
-      input.add(input.get(0));
-      input.remove(0);
+      input.remove(0);//runs 1 time
+      input.add(input.get(0));//runs 2 times
+      input.remove(0);//runs 1 time
     }
-    return input.get(0);
+    return input.get(0);//runs 2 times
   }
 
 }
+
+//The total is n(2+1+2+1)+2= 6n+2, so the time complexity is O(n).
