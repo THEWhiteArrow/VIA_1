b2c5ee08a984f55a6902fee11a573fc9b7779bfd
gramovi4a <331957@viauc.dk>
2022-11-02 14:18:35 +0100

    2 exercise

--- BalancedParenthesisChecking/parenthesis.java
+++ BalancedParenthesisChecking/parenthesis.java
@@ -6,7 +6,34 @@ public class parenthesis
 {
   public boolean checkParentheses(ArrayList<Character> input)
   {
-    // TODO Insert your method here and update the return statement
-    return false;
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < input.size(); i++) {
+      if (input.get(i)== '(' || input.get(i) == '[')
+      {
+        stack.push(input.get(i));
+      }
+      else if (stack.isEmpty()) {
+        return false;
+      }
+      else
+      {
+       char check;
+        switch (input.get(i))
+        {
+          case ')':
+            check = stack.pop();
+            if (check == '[')
+              return false;
+            break;
+
+          case ']':
+            check = stack.pop();
+            if (check == '(')
+              return false;
+            break;
+        }
+        }
+      }
+    return (stack.isEmpty());
   }
 }
---
+++ out/production/VIA_1/README.md
@@ -0,0 +1,2 @@
+# VIA_1
+Github repository for 1st semester group.
