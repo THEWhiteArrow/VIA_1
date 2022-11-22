32e68373eeae19c05a18b9dc27f4b66f82633f90
grumblewoes <81644977+grumblewoes@users.noreply.github.com>
2022-11-22 18:30:19 +0100

    Catarina's solution

--- HANDIN_7/src/inversions/Inversions.java
+++ HANDIN_7/src/inversions/Inversions.java
@@ -1,13 +1,31 @@
-package inversions;
-
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
 
 public class Inversions
 {
-  public int countInversions(ArrayList<Integer> input) {
-        // Implement your code here to return the number of inversions in input!
-        return 42;
+  public int countInversions(ArrayList<Integer> input)
+  {
+    //initializes and assigns = 2
+    ArrayList<Integer> invList = new ArrayList<Integer>(input);
+
+
+    //initializes and assigns = 2
+    int counter=0;
+
+    //outer loop runs n times
+    for (int i=0; i<invList.size()-1; i++){
+      //inner loop runs n times per iteration of outer loop
+      for (int j=i ; j<invList.size(); j++)
+      {
+        //2 methods, 1 comparison = 3
+        if (invList.get(i) > invList.get(j))
+        {
+          counter ++; //increment = 1
+        }
+
+      }
     }
+    return counter; //return = 1
+  }
 }
 
+// T(n) = 2+2+n*n(3+1)+1 --> 5 + 4n^2 = O(n^2)
